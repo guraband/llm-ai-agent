ed12738f9fcc2ac8c0da8f6e74f172a5fc14b322
NG <guraband@naver.com>
2025-07-12 21:42:45 +0900

    chapter04: PDF 텍스트 추출 및 전처리 스크립트 추가
    - pdf_to_txt.py: PDF 파일의 전체 텍스트를 추출하여 .txt 파일로 저장하는 스크립트 추가
    - pdf_without_header_footer.py: PDF에서 헤더/푸터를 제외한 본문만 추출하여 저장하는 전처리 스크립트 추가

---
+++ chapter04/pdf_to_txt.py
@@ -0,0 +1,20 @@
+import pymupdf
+import os
+
+pdf_file_path = "../doc/과정기반 작물모형을 이용한 웹 기반 밀 재배관리 의사결정 지원시스템 설계 및 구축.pdf"
+doc = pymupdf.open(pdf_file_path)
+
+full_txt = ''
+
+for page in doc:
+    text = page.get_text()
+    full_txt += text
+
+pdf_file_name = os.path.basename(pdf_file_path)
+pdf_file_name = os.path.splitext(pdf_file_name)[0]  # 확장자 제거
+
+txt_file_path = f"../output/{pdf_file_name}.txt"
+with open(txt_file_path, 'w', encoding='utf-8') as f:
+    f.write(full_txt)
+
+print(f"파일 저장 완료: {txt_file_path}")
